f3c0cb10d3706605bfcbe0e2e537a428bcffba4a
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-03-07 22:05:34 +0000

    Replaced script with npm package

--- scriptable.sh
+++ scriptable.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function show_commands() {
 	echo "Available commands: '$1'"
 	printf '\r\n'
@@ -9,37 +8,26 @@ function show_commands() {
 	printf '\r\n'
 }
 
-
-function upgradeTypes() {
-	curl https://raw.githubusercontent.com/schl3ck/ios-scriptable-types/master/dist/scriptable.d.ts -o index.d.ts
-}
-
-
 function init() {
 	ln -s ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/ build
 }
-
 
 function import() {
 	ln ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/"$1.js" "widgets/$1.js"
 }
 
-
 function importModule() {
 	ln ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/modules/"$1.js" "modules/$1.js"
 }
-
 
 function list() {
 	ls ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/
 }
 
-
-commands=(init list import importModule upgradeTypes)
-
+commands=(init list import importModule)
 
 if [[ $# -gt 0 ]] && [[ "${commands[@]}" =~ "$1" ]]; then
-	$1 "${@:2}";
+	$1 "${@:2}"
 else
 	show_commands "$commands"
 fi
