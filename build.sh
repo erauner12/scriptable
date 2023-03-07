8b0fa9a648f753618e2171994a8b18f780f6feff
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-03-07 21:57:19 +0000

    Allow spaces in file name and path

--- build.sh
+++ build.sh
@@ -16,17 +16,48 @@ has_param() {
 build() {
   local entry_file_path=$1
   local parsed_path
+
   if [[ $entry_file_path == ./src/* ]]; then
     parsed_path=$entry_file_path
   else
-    parsed_path=./src/entry/$entry_file_path.ts
+    parsed_path=$(find . -name "$1.ts")
   fi
+
+  local escape_spaces=$(parse_path "$parsed_path")
+
   local cmd
-  cmd="rollup --config rollup.config.ts --environment file_path:$parsed_path --configPlugin @rollup/plugin-typescript"
+  cmd="rollup --config rollup.config.ts --environment file_path:"$escape_spaces" --configPlugin @rollup/plugin-typescript"
   if has_param '--watch' "$@"; then cmd+=' --watch'; fi
   $cmd
 }
 
 build_and_watch() {
   build "$1" --watch
+}
+
+function parse_path() {
+  local separator="/"
+  local encoded_array=()
+
+  IFS=$separator
+  read -Ar strarr <<<$1
+
+  for value in "${strarr[@]}"; do
+    current_val=$(echo $value | perl -MURI::Escape -ne 'chomp;print uri_escape($_),"\n"')
+    encoded_array+=($current_val)
+  done
+
+  echo -n "$(join_array "/" "${encoded_array[@]}")"
+}
+
+# Join an array of elements into a string with a separator
+function join_array() {
+  local separator="$1"
+  shift
+  local -a elements=("$@")
+  local result="${elements[0]}"
+  for ((i = 1; i < ${#elements[@]}; i++)); do
+    result="${result}${separator}${elements[i]}"
+  done
+  echo -n "$result"
 }
--- rollup.config.ts
+++ rollup.config.ts
@@ -5,13 +5,14 @@ import addFileIconSettings from "./rollup-plugin-add-file-icon-settings";
 
 // https://github.com/rollup/rollup/issues/703#issuecomment-224984436 <-- passing args into config file
 const ENTRY_FILE_PATH = process.env.file_path;
+const DECODED_FILE_PATH = ENTRY_FILE_PATH ? decodeURI(ENTRY_FILE_PATH) : null;
 
 const config = {
-	input: ENTRY_FILE_PATH,
+	input: DECODED_FILE_PATH,
 	output: {
 		dir: "dist",
 		format: "es",
-		plugins: [terser(), addFileIconSettings(ENTRY_FILE_PATH!)],
+		plugins: [terser(), addFileIconSettings(DECODED_FILE_PATH!)],
 	},
 	plugins: [typescript(), nodeResolve()],
 	watch: {
