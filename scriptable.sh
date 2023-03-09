0d0a6df482e6e71b3d86936d5a34366c0d75f824
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-03-09 04:03:05 +0000

    Improve import function adding cli menu selection

--- scriptable.sh
+++ scriptable.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+build_path="$(pwd)/dist/"
+module_path="$(pwd)/modules/"
+icloud_path="$HOME/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
+
 function show_commands() {
 	echo "Available commands: '$1'"
 	printf '\r\n'
@@ -9,11 +13,29 @@ function show_commands() {
 }
 
 function init() {
-	ln -s ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/ build
+	ln -s "${icloud_path}" build
 }
 
 function import() {
-	ln ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/"$1.js" "widgets/$1.js"
+	echo
+	local script=$(select_script "${build_path}")
+	if [[ $script == "exit" ]]; then
+		echo "🚪 Exiting script..."
+	else
+		local filename="$(find "${script}" -maxdepth 0 -type f -name "*.js" -exec basename {} \;)"
+		local link_path="$script"
+
+		if [ -e "$link_path" ]; then
+			remove_file "$link_path"
+			if [ "$?" != 0 ]; then
+				echo "🚪 Exiting script..."
+				return 1
+			fi
+		fi
+
+		ln "dist/${filename}" "$link_path"
+		echo "✅ Successfully created hard link: \"$link_path\""
+	fi
 }
 
 function importModule() {
@@ -22,6 +44,52 @@ function importModule() {
 
 function list() {
 	ls ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/
+}
+
+function select_script() {
+	local file_list="$(find "$1" -maxdepth 1 -type f -name "*.js" -exec basename {} \; | sort -f)"
+
+	local file_array=()
+	while read -r file; do
+		local filename="${file%.js}"
+		file_array+=("📝 ${filename}")
+	done <<<"$file_list"
+
+	# Add option to exit
+	file_array+=("🚪 Exit")
+
+	PS3=$'\n🤔 Select a script from the list (or enter 0 to exit): '
+	select file_name in "${file_array[@]}"; do
+		if [[ "$REPLY" == 0 ]] || [[ "$REPLY" == ${#file_array[@]} ]]; then
+			echo "exit"
+			return 1
+		fi
+
+		if [[ -n "$file_name" ]]; then
+			echo "$(find "${icloud_path}${file_name:2}.js" -maxdepth 0 -type f)"
+			return 0
+		fi
+	done
+}
+
+function remove_file() {
+	local file="$1"
+	local remove_file=""
+
+	while [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; do
+		read -n 1 -p "😱 File already exists. Do you want to remove it? [y/n]: " remove_file
+		echo
+
+		if [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; then
+			echo -e "\n✋ Invalid input. Please enter 'y' or 'n'."
+		fi
+	done
+
+	if [ "$remove_file" == "y" ]; then
+		rm "$file"
+	else
+		return 1
+	fi
 }
 
 commands=(init list import importModule)
