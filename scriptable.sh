9927918a2a8c42d9dc6c6cc20155ffc1014eac72
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-03-09 15:48:57 +0000

    Extend functionality and robustness of functions

--- scriptable.sh
+++ scriptable.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-build_path="$(pwd)/dist/"
-module_path="$(pwd)/modules/"
-icloud_path="$HOME/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
+DIST_PATH="$(pwd)/dist/"
+MODULE_PATH="$(pwd)/modules/"
+BUILD_PATH="$(pwd)/build/"
+ICLOUD_PATH="$HOME/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
 
 function show_commands() {
 	echo "Available commands: '$1'"
@@ -13,29 +14,15 @@ function show_commands() {
 }
 
 function init() {
-	ln -s "${icloud_path}" build
+	local dir_name=$(basename "$BUILD_PATH")
+	local directory_path="$(pwd)/${dir_name}"
+
+	create_link "${ICLOUD_PATH}" "${directory_path}" -s
+	log_complete $?
 }
 
 function import() {
-	echo
-	local script=$(select_script "${build_path}")
-	if [[ $script == "exit" ]]; then
-		echo "🚪 Exiting script..."
-	else
-		local filename="$(find "${script}" -maxdepth 0 -type f -name "*.js" -exec basename {} \;)"
-		local link_path="$script"
-
-		if [ -e "$link_path" ]; then
-			remove_file "$link_path"
-			if [ "$?" != 0 ]; then
-				echo "🚪 Exiting script..."
-				return 1
-			fi
-		fi
-
-		ln "dist/${filename}" "$link_path"
-		echo "✅ Successfully created hard link: \"$link_path\""
-	fi
+	select_and_link "$DIST_PATH" "$BUILD_PATH" ".js"
 }
 
 function importModule() {
@@ -46,12 +33,103 @@ function list() {
 	ls ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/
 }
 
+function create_link() {
+	local source_path="$1"
+	local destination_path="$2"
+	local soft_flag="$3"
+	local link_type="hard"
+	local friendly_source_path=$(get_user_friendly_path "$source_path")
+	local friendly_destination_path=$(get_user_friendly_path "$destination_path")
+
+	if [ ! -d "$(dirname "$source_path")" ]; then
+		log_error "Source directory \"$(dirname "$friendly_source_path")\" does not exist"
+	fi
+
+	if [ ! -d "$(dirname "$destination_path")" ]; then
+		log_error "Destination directory \"$(dirname "$friendly_destination_path")\" does not exist"
+	fi
+
+	if [ "$?" != 0 ]; then
+		return 1
+	fi
+
+	local ln_command="ln \"$source_path\" \"$destination_path\""
+
+	if [ "$soft_flag" == "-s" ]; then
+		ln_command="ln -s \"$source_path\" \"$destination_path\""
+		link_type="soft"
+	fi
+
+	check_link "$destination_path"
+	eval $ln_command 2>/dev/null
+
+	if [ "$?" == 0 ]; then
+		log_success "Successfully created $link_type link: \"$friendly_source_path\" to \"$friendly_destination_path\""
+	else
+		log_error "Failed to create $link_type link: \"$friendly_source_path\" to \"$friendly_destination_path\""
+	fi
+}
+
+function check_link() {
+	local path="$1"
+	local dir_name=$(basename "$directory_path")
+	local path_user_friendly=$(get_user_friendly_path "$path")
+
+	if [ -e "${path}" ]; then
+		if [[ -L "${path}" ]]; then
+			log_error "\"$path_user_friendly\" is already a symbolic link..."
+		else
+			log_error "\"$path_user_friendly\" is not a symbolic link..."
+		fi
+	else
+		return 0
+	fi
+}
+
+function select_and_link() {
+	local source_path="$1"
+	local destination_path="$2"
+	local search_extension="$3"
+	local soft_link="$4"
+
+	echo
+	local script_path=$(select_script "$source_path" "$search_extension")
+	if [[ $script_path == "exit" ]]; then
+		log_complete 1
+	fi
+
+	local filename="$(find "${script_path}" -maxdepth 0 -type f -exec basename {} \;)"
+
+	if [ -z "$filename" ]; then
+		log_error "No script file found: \"$script_path\""
+	else
+		local link_path="$destination_path$filename"
+	fi
+
+	if [ -e "$link_path" ]; then
+		check_file_exists "$link_path"
+	fi
+
+	if [ "$?" == 0 ]; then
+		create_link "$script_path" "$link_path" "$soft_link"
+	fi
+
+	log_complete $?
+}
+
 function select_script() {
-	local file_list="$(find "$1" -maxdepth 1 -type f -name "*.js" -exec basename {} \; | sort -f)"
+	local script_dir="$1"
+	local file_extension="$2"
+	local file_list="$(find "$script_dir" -maxdepth 1 -type f -name "*$file_extension" -exec basename {} \; | sort -f)"
+
+	if [[ -z "$file_list" ]]; then
+		echo "🚫 No script files found in \"$script_dir\"..."
+		return 1
+	fi
 
 	local file_array=()
 	while read -r file; do
-		local filename="${file%.js}"
+		local filename="${file%$file_extension}"
 		file_array+=("📝 ${filename}")
 	done <<<"$file_list"
 
@@ -66,29 +144,81 @@ function select_script() {
 		fi
 
 		if [[ -n "$file_name" ]]; then
-			echo "$(find "${icloud_path}${file_name:2}.js" -maxdepth 0 -type f)"
+			local selected_file="$script_dir${file_name:2}$file_extension"
+			echo "$selected_file"
 			return 0
 		fi
 	done
 }
 
-function remove_file() {
-	local file="$1"
+function check_file_exists() {
+	local path="$1"
+	local path_user_friendly=$(get_user_friendly_path "$path")
 	local remove_file=""
 
-	while [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; do
-		read -n 1 -p "😱 File already exists. Do you want to remove it? [y/n]: " remove_file
-		echo
+	if [ -e "$path" ]; then
+		check_link "$path"
+		while [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; do
+			echo "😱 \"$path_user_friendly\" already exists..."
+			read -p "🤔 Do you want to remove it? [y/n]: " remove_file
 
-		if [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; then
-			echo -e "\n✋ Invalid input. Please enter 'y' or 'n'."
-		fi
-	done
+			if [[ "$remove_file" != "y" && "$remove_file" != "n" ]]; then
+				echo -e "\n✋ Invalid input. Please enter 'y' or 'n'"
+			fi
+		done
+	else
+		echo "🔍 File not found at \"$path_user_friendly\""
+		return 1
+	fi
 
 	if [ "$remove_file" == "y" ]; then
-		rm "$file"
+		echo "🔥 Removing \"$path_user_friendly\"..."
+		rm "$path"
+
+		# Check if the file still exists
+		if [ -e "$path" ]; then
+			log_error "Failed to remove \"$path_user_friendly\""
+		else
+			log_success "Removed \"$path_user_friendly\""
+		fi
 	else
+		log "Leaving \"$path_user_friendly\" in place..."
 		return 1
+	fi
+
+}
+
+function get_user_friendly_path() {
+	local path="$1"
+	echo "${path/$(pwd)/~}"
+}
+
+function log() {
+	local message="$1"
+	echo "✨ $message"
+}
+
+function log_success() {
+	local message="$1"
+	echo "✅ $message"
+	return 0
+}
+
+function log_error() {
+	local message="$1"
+	echo "❌ $message"
+	return 1
+}
+
+function log_complete() {
+	local exit_code=$1
+
+	if [[ $exit_code -ne 0 ]]; then
+		echo "🚪 Exiting script..."
+		exit $exit_code
+	else
+		echo "🚀 Done!"
+		exit 0
 	fi
 }
 
