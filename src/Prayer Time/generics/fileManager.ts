62f91f4f4d5dfdcd23fbefeed6b182f3fb57fea4
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-02-18 01:43:15 +0000

    Add return types

--- src/Prayer Time/generics/fileManager.ts
+++ src/Prayer Time/generics/fileManager.ts
@@ -1,5 +1,5 @@
 // Loads data stored in the .json file
-export async function loadData(path: string) {
+export async function loadData(path: string): Promise<any> {
 	let fm = FileManager.iCloud();
 
 	if (fm.fileExists(path)) {
@@ -7,14 +7,11 @@ export async function loadData(path: string) {
 		const raw = fm.readString(path);
 		const data = JSON.parse(raw);
 		return data;
-	} else {
-		const data: any = [];
-		return data;
 	}
 }
 
 // Saves the data to a file in iCloud Drive
-export function saveData(path: string, data: object) {
+export function saveData(path: string, data: object): void {
 	let fm = FileManager.iCloud();
 	let raw = JSON.stringify(data, null, 2);
 	fm.writeString(path, raw);
