f1d1369f1cf8cc3c3a077d34f4c70bc1f9502db9
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-06 23:38:18 +0000

    Remove Lockscreen Text.js

--- dist/Lockscreen Text.js
+++
@@ -1,4 +0,0 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: archive;
-let e=new ListWidget;e.addStack().addText(Script.name()).centerAlignText(),config.runsInAccessoryWidget?Script.setWidget(e):e.presentSmall();
