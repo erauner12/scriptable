048db61049a5af3972cfad35d2c41426f6316cdb
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-17 15:15:00 +0000

    Add DeepPartial type helper

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -3,3 +3,7 @@ export type Prettify<T> = {
 } & {};
 
 export type NonEmptyArray<T> = [T, ...T[]];
+
+export type DeepPartial<T> = {
+	[P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
