0d15abfdc0cc4e04fe78f081f3b48fab97cebf46
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-17 14:30:06 +0000

    Add getWidgetParameters()

---
+++ src/utilities/scriptable/getWidgetParameters.ts
@@ -0,0 +1,18 @@
+export function getWidgetParameters<T>(
+	widgetParameters: any,
+	delimiter: string,
+	userSettingsCallback: (parsedWidgetParameters: string[]) => Partial<T>
+): Partial<T> {
+	if (
+		typeof widgetParameters === "string" &&
+		widgetParameters.trim().length > 0
+	) {
+		const parsedWidgetParameters = widgetParameters
+			.split(delimiter)
+			.map((parameter) => parameter.trim());
+
+		return userSettingsCallback(parsedWidgetParameters);
+	}
+
+	return {};
+}
