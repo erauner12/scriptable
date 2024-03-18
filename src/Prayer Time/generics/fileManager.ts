f6e23f5b318a9225bf876e1a51dd593c7d44b901
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-18 18:40:57 +0000

    Add `getFilePath()`

--- src/Prayer Time/generics/fileManager.ts
+++ src/Prayer Time/generics/fileManager.ts
@@ -16,3 +16,17 @@ export function saveData(path: string, data: object): void {
 	let raw = JSON.stringify(data, null, 2);
 	fm.writeString(path, raw);
 }
+
+// Gets the path of the file containing the stored "Prayer Time" data. Creates the file if it doesn't exist.
+export function getFilePath(fileName: string, directoryName: string) {
+	const file = `${fileName}.json`;
+	const directory = directoryName;
+	const fm = FileManager.iCloud();
+	const dirPath = fm.joinPath(fm.documentsDirectory(), directory);
+
+	if (!fm.fileExists(dirPath)) {
+		fm.createDirectory(dirPath);
+	}
+
+	return fm.joinPath(dirPath, file);
+}
