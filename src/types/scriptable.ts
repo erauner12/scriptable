b73aa2d760050fe38095696473d6393d8f7635f7
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-18 14:01:27 +0000

    Add RequestProperties type

--- src/types/scriptable.ts
+++ src/types/scriptable.ts
@@ -1,5 +1,8 @@
+import type { PropertiesOf } from "src/types/helpers";
 import { getRunLocation } from "src/utilities/scriptable/getRunLocation";
 
 export type WidgetSize = NonNullable<typeof config.widgetFamily>;
 
 export type RunLocation = ReturnType<typeof getRunLocation>;
+
+export type RequestProperties = Pick<Request, PropertiesOf<Request>>;
