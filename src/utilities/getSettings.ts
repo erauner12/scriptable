7b009a9b57222cb30f5cda24c4f14312c6c8b6a4
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-24 23:58:50 +0000

    Add mergeDeep()

--- src/utilities/getSettings.ts
+++ src/utilities/getSettings.ts
@@ -5,6 +5,29 @@ export function getSettings<T>(
 	userSettings: DeepPartial<T> | ((defaultSettings: T) => DeepPartial<T>),
 	overrideSettings?: DeepPartial<T>,
 ): T {
-	const settings: DeepPartial<T> = typeof userSettings === "function" ? userSettings(defaultSettings) : userSettings;
-	return { ...defaultSettings, ...settings, ...overrideSettings };
+	const settingsToApply: DeepPartial<T> = typeof userSettings === "function" ? userSettings(defaultSettings) : userSettings;
+	const mergedSettings = mergeDeep(Object.assign({}, defaultSettings), settingsToApply);
+
+	if (overrideSettings) {
+		return mergeDeep(mergedSettings, overrideSettings);
+	}
+
+	return mergedSettings;
+}
+
+function mergeDeep<T>(target: T, source: DeepPartial<T>): T {
+	for (const key in source) {
+		const targetValue = target[key];
+		const sourceValue = source[key];
+
+		if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+			target[key] = sourceValue as any; // replace arrays, not merge them
+		} else if (typeof targetValue === "object" && typeof sourceValue === "object") {
+			target[key] = mergeDeep(Object.assign({}, targetValue), sourceValue as any);
+		} else {
+			target[key] = sourceValue as any;
+		}
+	}
+
+	return target;
 }
