7d918480386ccbf5e05a8acd3e4763c70405e972
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-28 22:09:07 +0000

    Refactor isOnline function to use WebView for checking online status

--- src/utilities/scriptable/common/isOnline.ts
+++ src/utilities/scriptable/common/isOnline.ts
@@ -1,16 +1,8 @@
-import { fetchRequest } from "src/utilities/scriptable/common";
-
-export async function isOnline(timeoutIntervalMs: number = 1000): Promise<boolean> {
-	const url = "https://www.google.com";
-
+export async function isOnline(): Promise<boolean> {
 	try {
-		const timeoutIntervalSeconds = timeoutIntervalMs / 1000;
-		const response = await fetchRequest(
-			url,
-			{},
-			{ timeoutInterval: timeoutIntervalSeconds, method: "HEAD" },
-			async (request) => await request.load(),
-		);
+		const webView = new WebView();
+		const javaScript = "navigator.onLine";
+		const response = await webView.evaluateJavaScript(javaScript);
 		if (response) return true;
 		return false;
 	} catch (error) {
