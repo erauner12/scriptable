62298632f4ec1ba7c5e9a4e8dc28e312e7c46f93
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-28 21:49:26 +0000

    Fix timeout interval in isOnline function

--- src/utilities/scriptable/common/isOnline.ts
+++ src/utilities/scriptable/common/isOnline.ts
@@ -1,11 +1,15 @@
 import { fetchRequest } from "src/utilities/scriptable/common";
 
-export async function isOnline(timeoutIntervalMs: number = 15): Promise<boolean> {
+export async function isOnline(timeoutIntervalMs: number = 1000): Promise<boolean> {
 	const url = "https://www.google.com";
 
 	try {
-		const response = await fetchRequest(url, {}, { timeoutInterval: timeoutIntervalMs / 1000, method: "HEAD" }, async (request) =>
-			request.load(),
+		const timeoutIntervalSeconds = timeoutIntervalMs / 1000;
+		const response = await fetchRequest(
+			url,
+			{},
+			{ timeoutInterval: timeoutIntervalSeconds, method: "HEAD" },
+			async (request) => await request.load(),
 		);
 		if (response) return true;
 		return false;
