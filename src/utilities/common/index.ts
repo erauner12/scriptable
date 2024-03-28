8ce1012122dd2f8afe54df13901ef451379c0a00
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-28 14:17:41 +0000

    Add common folder for utility functions and export from index

---
+++ src/utilities/common/index.ts
@@ -0,0 +1,4 @@
+export * from "src/utilities/common/getSettings";
+export * from "src/utilities/common/getters";
+export * from "src/utilities/common/handleError";
+export * from "src/utilities/common/mergeDeep";
